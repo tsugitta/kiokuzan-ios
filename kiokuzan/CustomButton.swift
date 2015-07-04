4d7002958811345a6e2039f5f4681bc9f606f3c1
tsugitta <toshi.sdkl@gmail.com>
2015-07-04 18:48:14 +0900

    Add custom button

---
+++ kiokuzan/CustomButton.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+@IBDesignable
+class CustomButton: UIButton {
+  
+  @IBInspectable var textColor: UIColor?
+  
+  @IBInspectable var cornerRadius: CGFloat = 0 {
+    didSet {
+      layer.cornerRadius = cornerRadius
+    }
+  }
+  
+  @IBInspectable var borderWidth: CGFloat = 0 {
+    didSet {
+      layer.borderWidth = borderWidth
+    }
+  }
+  
+  @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+    didSet {
+      layer.borderColor = borderColor.CGColor
+    }
+  }
+}

2b6f354d2c69cb1c9daca2b203f5def0df7d9fa8
tsugitta <toshi.sdkl@gmail.com>
2015-07-07 03:25:54 +0900

    Add inputBoard basis

---
+++ kiokuzan/InputBoard.swift
@@ -0,0 +1,21 @@
+//
+//  InputBoard.swift
+//  kiokuzan
+//
+//  Created by sdklt on 2015/07/04.
+//  Copyright (c) 2015年 sdklt. All rights reserved.
+//
+
+import UIKit
+
+class InputBoard: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
